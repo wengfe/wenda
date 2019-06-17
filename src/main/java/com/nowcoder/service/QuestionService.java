f022cdcf4cbaea1007c4ab6985ba0c318e205bb1
wengfuyi <214952026@qq.com>
2019-06-17 14:52:07 +0800

    提问后端代码

--- src/main/java/com/nowcoder/service/QuestionService.java
+++ src/main/java/com/nowcoder/service/QuestionService.java
@@ -2,17 +2,30 @@ package com.nowcoder.service;
 
 import com.nowcoder.dao.QuestionDAO;
 import com.nowcoder.model.Question;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
 @Service
 public class QuestionService {
+    private static final Logger logger = LoggerFactory.getLogger(QuestionService.class);
+
     @Autowired
     QuestionDAO questionDAO;
 
-    public List<Question> getLatestQuestions(int id, int offset, int limit){
+    public int addQuestion(Question question) {
+//        html 过滤
+        question.setContent((HtmlUtils.htmlEscape(question.getContent())));
+        question.setTitle(HtmlUtils.htmlEscape(question.getTitle()));
+//        敏感词过滤
+        return questionDAO.addQuestion(question) > 0 ? question.getId() : 0;
+    }
+
+    public List<Question> getLatestQuestions(int id, int offset, int limit) {
         return questionDAO.selectLatestQuestions(id, offset, limit);
     }
 }
--- src/main/java/com/nowcoder/util/WendaUtil.java
+++ src/main/java/com/nowcoder/util/WendaUtil.java
@@ -1,5 +1,6 @@
 package com.nowcoder.util;
 
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,8 +9,23 @@ import java.security.MessageDigest;
 public class WendaUtil {
     private static final Logger logger = LoggerFactory.getLogger(WendaUtil.class);
 
-    public static String MD5(String key){
-        char hexDigits[]={
+    public static int ANONYMOUS_USERID = 3;
+
+    public static String getJSONString(int code, String msg) {
+        JSONObject json = new JSONObject();
+        json.put("code", code);
+        json.put("msg", msg);
+        return json.toJSONString();
+    }
+
+    public static String getJSONString(int code) {
+        JSONObject json = new JSONObject();
+        json.put("code", code);
+        return json.toJSONString();
+    }
+
+    public static String MD5(String key) {
+        char hexDigits[] = {
                 '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
         };
         try {
@@ -24,7 +40,7 @@ public class WendaUtil {
             int j = md.length;
             char str[] = new char[j * 2];
             int k = 0;
-            for (int i = 0; i < j; i++){
+            for (int i = 0; i < j; i++) {
                 byte byte0 = md[i];
                 str[k++] = hexDigits[byte0 >>> 4 & 0xf];
                 str[k++] = hexDigits[byte0 & 0xf];
